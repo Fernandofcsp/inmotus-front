3473c4cc31c9c61b4e9a950eb6ac7fa4a591f38d
Fernando Cesar Sandoval Padilla <fernando.sandoval6854@alumnos.udg.mx>
2026-03-02 19:06:19 -0600

    fix: add trailing slash to base path to fix asset URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fernandofcsp.github.io',
-  base: '/inmotus-front',
+  base: '/inmotus-front/',
   integrations: [react(), tailwind()],
 });
