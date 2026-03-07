55924f4ed383232cf5a94212f84c19621a3e749c
Fernando Cesar Sandoval Padilla <fernando.sandoval6854@alumnos.udg.mx>
2026-03-07 13:43:42 -0600

    Add sitemap integration and robots.txt for SEO optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://inmotus.mx',
   base: '/',
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   vite: {
     ssr: {
       external: ['node:crypto'],
